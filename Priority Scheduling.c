4257d935cb18b42d91c4adaf4147fb5e96e9c3a0
erdemcil <erdemchill@gmail.com>
2016-10-23 15:27:34 +0300

    First Commit

---
+++ Priority Scheduling.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+
+void main()
+{
+    int burstTime[10],p[10],waitingtTime[10],turnAroundTime[10],priority[10];
+    int i,j,temp,numOfProcess,total=0,k,averageWaitingTime;
+    do{
+        printf("Number of processes:");
+        scanf("%d",&numOfProcess);
+        if (numOfProcess>10 || numOfProcess <1)
+        {
+            printf("Please enter a process number between 1 and 10 !!!\n");
+            printf("\n");
+        }
+    }
+    while(numOfProcess>10 || numOfProcess <1);
+
+    for(i = 0; i < numOfProcess; i++)
+  {
+      printf("\nEnter Burst Time for per Processor P(%d): ",i+1);
+      scanf("%d",&burstTime[i]);
+      printf("Enter Priority   for per Processor P(%d): ",i+1);
+      scanf("%d",&priority[i]);
+      p[i]=i+1;
+  }
+
+    for(i=0;i<numOfProcess;i++)
+    {
+        k=i;
+        for(j=i+1;j<numOfProcess;j++)
+        {
+            if(priority[j]<priority[k])
+                k=j;
+        }
+
+        temp=priority[i];
+        priority[i]=priority[k];
+        priority[k]=temp;
+        temp=burstTime[i];
+        burstTime[i]=burstTime[k];
+        burstTime[k]=temp;
+        temp=p[i];
+        p[i]=p[k];
+        p[k]=temp;
+    }
+
+    waitingtTime[0]=0;
+
+    for(i=1;i<numOfProcess;i++)
+    {
+        waitingtTime[i]=0;
+        for(j=0;j<i;j++)
+            waitingtTime[i]+=burstTime[j];
+        total += waitingtTime[i];
+    }
+
+    averageWaitingTime = total/numOfProcess;
+    total=0;
+
+    printf("\nProcess Number  Burst Time      Priority         Waiting Time  TurnAround Time\n");
+
+    for(i=0;i<numOfProcess;i++)
+    {
+        turnAroundTime[i] = burstTime[i] + waitingtTime[i];
+        total+=turnAroundTime[i];
+        printf("\nP[%d]\t\t%d\t\t%d\t\t  %d   \t\t%d",p[i],burstTime[i],priority[i],waitingtTime[i],turnAroundTime[i]);
+    }
+
+    printf("\n\nAverage Waiting Time =%d",averageWaitingTime);
+    printf("\n");
+
+}
---
+++ Round Robin (RR).c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+int main()
+{
+
+  int numOfProcess,remaining, flag = 0,time,quantum,i,j;
+  int waitingtTime = 0,turnAroundTime = 0,arrivalTime[10],burstTime[10],responseTime[10];
+  do{
+        printf("Number of processes:");
+        scanf("%d",&numOfProcess);
+        if (numOfProcess>10 || numOfProcess <1)
+        {
+            printf("Please enter a process number between 1 and 10 !!!\n");
+            printf("\n");
+        }
+    }
+    while(numOfProcess>10 || numOfProcess <1);
+  remaining = numOfProcess;
+  for(i = 0; i < numOfProcess; i++)
+  {
+    printf("\nEnter Arrival Time for per Processor P(%d): ",i+1);
+    scanf("%d",&arrivalTime[i]);
+    printf("Enter Burst   Time for per Processor P(%d): ",i+1);
+    scanf("%d",&burstTime[i]);
+
+    responseTime[i]=burstTime[i];
+  }
+  printf("\nEnter the time of Quantum: ");
+  scanf("%d",&quantum);
+  printf("\nProcess Number  Burst Time\tWaiting Time    TurnAround Time   Arrival Time\n");
+
+  for(time = 0,i = 0; remaining != 0;)
+  {
+    if(responseTime[i]<=quantum && responseTime[i]>0)
+    {
+      time += responseTime[i];
+      responseTime[i] = 0;
+      flag = 1;
+    }
+    else if(responseTime[i]>0)
+    {
+      responseTime[i] -= quantum;
+      time += quantum;
+    }
+    if(responseTime[i]==0 && flag==1)
+    {
+      remaining--;
+      printf("\nP(%d)   \t\t%d\t\t%d\t\t%d\t\t  %d",i+1, burstTime[i],time - arrivalTime[i] - burstTime[i], time - arrivalTime[i], arrivalTime[i]);
+      waitingtTime += time - arrivalTime[i] - burstTime[i];
+      turnAroundTime += time - arrivalTime[i];
+      flag=0;
+    }
+    if(i == numOfProcess - 1)
+      i=0;
+    else if(arrivalTime[i+1] <= time)
+      i++;
+    else
+      i=0;
+  }
+  printf("\n\nAverage Waiting Time = %.2f\n",waitingtTime* 1.0 / numOfProcess);
+
+  return 0;
+}
